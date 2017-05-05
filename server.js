817cf7ae51e773bdcaf9a2ccc0fba2a3af614152
Kiran Kumaar S <imkikus@gmail.com>
2017-05-05 15:04:16 +0530

    CHanges to routes

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
-app.get('*', function(req, res) {
-  res.render('index.html');
-});
+// app.get('*', function(req, res) {
+//   res.render('index.html');
+// });
 app.listen(port);
 console.log('server started ' + port);
