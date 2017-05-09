2bb7d5779693d292151cf65d755a2e94d845aa34
Kiran Kumaar S <imkikus@gmail.com>
2017-05-09 12:44:53 +0530

    Changes to view static file path

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 // app.use(serveStatic(__dirname));
-app.set('view engine', 'html');
+
+app.set('view options', {layout: false});
+// app.set('view engine', 'html');
 app.use(express.static(__dirname));
 var port = process.env.PORT || 5000;
 app.get('*', function(req, res) {
