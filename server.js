ef8f380e1153d8e7fac1a85c18e2cae60b9ca0ec
Kiran Kumaar S <imkikus@gmail.com>
2017-05-09 12:30:30 +0530

    Changes in serverjs

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname));
+// app.use(serveStatic(__dirname));
+app.use(express.static(__dirname + '/static'));
 var port = process.env.PORT || 5000;
 app.get('*', function(req, res) {
-  res.render('index.html');
+  res.render('index');
 });
 app.listen(port);
 console.log('server started ' + port);
